dcacee33b7557c78a1429722463f1eb83b288f1f
juniorbalthazar <juniorbalthazar@yahoo.fr>
2025-11-02 22:10:29 -0500

    test first integration with auth

---
+++ target/generated-sources/annotations/ht/bms/applicant/service/mapper/ApplicantMapperImpl.java
@@ -0,0 +1,13 @@
+package ht.bms.applicant.service.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-02T21:22:02-0500",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
+)
+@Component
+public class ApplicantMapperImpl implements ApplicantMapper {
+}
